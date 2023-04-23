c716849ae0f8426a717d400ad89ac8b2b23c6bf2
wallacejoe <JAWcoder@gmail.com>
2023-04-23 00:26:06 -0700

    First vehicle page draft.

--- server.js
+++ server.js
@@ -8,11 +8,23 @@
 const express = require("express")
 const env = require("dotenv").config()
 const app = express()
+const expressLayouts = require("express-ejs-layouts")
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
 
 /* ***********************
  * Routes
  *************************/
 app.use(require("./routes/static"))
+// Index route
+app.get("/", function(req, res){
+  res.render("index", {title: "Home"})
+})
 
 /* ***********************
  * Local Server Information
