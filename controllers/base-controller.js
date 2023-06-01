9bc603b48615bed5f0871c5851f8d83a665f2d93
wallacejoe <JAWcoder@gmail.com>
2023-06-01 15:22:13 -0700

    Added flash message capability.

--- controllers/base-controller.js
+++ controllers/base-controller.js
@@ -4,6 +4,8 @@ const baseController = {}
 
 baseController.buildHome = async function(req, res){
   const nav = await utilities.getNav()
+  /*This code displays a "flash" message.*/
+  //req.flash("notice", "This is a flash message.")
   res.render("index", {title: "Home", nav})
 }
 
